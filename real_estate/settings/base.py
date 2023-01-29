6412e99550edbf819749e8584305076976a1bcd4
Mike <michael@gmail.com>
2023-01-29 20:06:59 +0300

    added logger to console.log error

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -114,10 +114,55 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIR = []
-MEDIA_URL = "/mediafiles"
+MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import logging
+import logging.config
+
+from django.utils.log import DEFAULT_LOGGING;
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+
+logging.config.dictConfig({
+    "version":1,
+    "disable_existing_loggers":False,
+    "formatters":{
+        "console":{
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "file":{"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers":{
+        "console":{
+            "class":"logging.StreamHandler",
+            "formatter":"console",
+        },
+        "file":{
+            "level":"INFO",
+            "class":"logging.FileHandler",
+            "formatter":"file",
+            "filename":"logs/real_estate.log",
+        },
+        "django.server":DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers":{
+        "":{"level":"INFO","handlers":["console","file"],"propagate":False},
+        "apps":{
+            "level":"INFO","handlers":["console"],"propagate":False
+        },
+        "django.server":DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+})
+
+
+

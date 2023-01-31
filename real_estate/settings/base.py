033e5d21d74adbed5fed146040f4b459a71e2444
Mike <michael@gmail.com>
2023-01-31 18:15:15 +0300

    added properties

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -41,7 +41,8 @@ THIRD_PARTY_APPS = [
     "djoser",
     "rest_framework_simplejwt",
     ]
-LOCAL_APPS = ["apps.common","apps.users","apps.profiles","apps.ratings"]
+LOCAL_APPS = ["apps.common","apps.users","apps.profiles","apps.ratings",
+"apps.properties","apps.enquiries",]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 

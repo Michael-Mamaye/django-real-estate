350079b7996df841d0e5de55bd0033999cf24609
Mike <michael@gmail.com>
2023-01-31 15:52:20 +0300

    added user crud api

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,7 +1,14 @@
 from .base import *
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= env('EMAIL_HOST')
+EMAIL_USE_TLS= True
+EMAIL_PORT= env('EMAIL_PORT')
+EMAIL_HOST_USER=env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL='info@real-estate.com'
+DOMAIN= env('DOMAIN')
+SITE_NAME= 'Real Estate'
 
 DATABASES = {
     'default': {
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -5,9 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('supersecret/', admin.site.urls),
+    path('api/v1/auth/',include("djoser.urls")),
+    path('api/v1/auth/',include("djoser.urls.jwt")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title ="Real Estate Admin Portal"
-admin.site.index_title ="Welcom to the Real Estate Portal"
+admin.site.index_title ="Welcome to the Real Estate Portal"
